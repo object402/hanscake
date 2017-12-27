3eeedb089a8f3c9d11f52df350868db01d1e5f31
meronaaa <33471288+meronaaa@users.noreply.github.com>
2017-12-27 15:40:15 +0900

    qsdasdasd

--- js/taste.js
+++ js/taste.js
@@ -19,23 +19,37 @@ $(document).ready(function() {
 
   $('.seasonalcake').click(function() {
     cakebox.fadeOut();
-    $('.seasonal').fadeIn(500);  $('.k_contents_nav > ul > li > a').removeClass('menuhover')
-      $(this).addClass('menuhover')
+    $('.seasonal').fadeIn(500);
+    $('.k_contents_nav > ul > li > a').removeClass('menuhover')
+    $(this).addClass('menuhover')
   })
   $('.premiumcake').click(function() {
     cakebox.fadeOut();
-    $('.premium').fadeIn(500);  $('.k_contents_nav > ul > li > a').removeClass('menuhover')
-      $(this).addClass('menuhover')
+    $('.premium').fadeIn(500);
+    $('.k_contents_nav > ul > li > a').removeClass('menuhover')
+    $(this).addClass('menuhover')
   })
   $('.xmascake').click(function() {
     cakebox.fadeOut();
-    $('.xmas').fadeIn(500);  $('.k_contents_nav > ul > li > a').removeClass('menuhover')
-      $(this).addClass('menuhover')
+    $('.xmas').fadeIn(500);
+    $('.k_contents_nav > ul > li > a').removeClass('menuhover')
+    $(this).addClass('menuhover')
   })
 
 
-
-
+  var cakecnt = $('.k_showcakes > ul > li').length;
+  for (var i = 0; i < cakecnt; i++) {
+    $('.k_showcakes > ul > li').eq(i).hover(function() {
+     $(this).children('dl').stop().fadeIn(400);
+     $(this).children('dl').children('dt').stop().animate({'top':45},'slow');
+     $(this).children('dl').children('dd').stop().animate({'bottom':0},'slow');
+   },function(){
+     $(this).children('dl').css({'display':'none'});
+     $(this).children('dl').children('dt').stop().animate({'top':-50});
+      $(this).children('dl').children('dd').stop().animate({'bottom':-150});
+   })
+    console.log(i);
+  }
 
 
 })
