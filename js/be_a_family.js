71093c267296b4366d9b51b38f77e612dc0ee9c2
999999sy <33650909+999999sy@users.noreply.github.com>
2017-12-28 17:52:17 +0900

    be a family end

---
+++ js/be_a_family.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+
+
+  $('.p_no').css({
+    "opacity": "0"
+  }).delay(300).animate({
+    "opacity": "1"
+  }, "slow");
+
+  $('.p_delay').css({
+    "opacity": "0"
+  }).delay(800).animate({
+    "opacity": "1"
+  }, "slow");
+
+
+
+})
